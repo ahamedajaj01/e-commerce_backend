bce9d7807e4e0945d74c06ebc4392c63d34e0374
ahamedajaj01 <ahamed.ajaj01@gmail.com>
2026-06-12 17:04:49 +0545

    updated model size

--- apps/catalog/models/product.py
+++ apps/catalog/models/product.py
@@ -66,8 +66,8 @@ class ProductVariant(BaseModel):
     )
     sku = models.CharField(max_length=100, unique=True)
     name = models.CharField(max_length=255, blank=True) # e.g. "Red / XL"
-    size = models.CharField(max_length=50, blank=True)
-    color = models.CharField(max_length=50, blank=True)
+    size = models.CharField(max_length=255, blank=True)
+    color = models.CharField(max_length=255, blank=True)
     price = models.DecimalField(max_digits=12, decimal_places=2)
     stock_quantity = models.IntegerField(default=0)
     is_active = models.BooleanField(default=True)
