3fc828e63ba9760c1a25aa1cc729b61dba999d6e
ahamedajaj01 <ahamed.ajaj01@gmail.com>
2026-06-12 16:14:20 +0545

    added storage configuration try 6

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -117,7 +117,9 @@ CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
     'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
-    'SECURE': True
+    'SECURE': True,
+    'MAGIC_FILE_PATH': True,
+    'PREFIX': os.getenv('MEDIA_URL', 'media/')
 }
 
 STORAGES = {
